6c7f87304122317239ccf135f5b85f499ac7f973
Harshit Mawri <98049556+harshit404540@users.noreply.github.com>
2025-05-26 18:56:30 +0530

    views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -10,9 +10,6 @@ import string
 def home(request):
 	return render(request,'home.html')
 
-def about(request):
-	return render(request,'about.html')
-
 l = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
